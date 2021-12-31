6e83a993b8653282f2f7e9eed160c2873095d258
xyxx22 <2651288299@qq.com>
2021-12-31 14:17:19 +0800

    vuex持久化

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
   <div>
     <div>app根组件</div>
-    <p>{{ $store.state.userName }}</p>
-    <p>{{ $store.getters.newName }}</p>
-    <button @click="btnChangeName">改变值</button>
   </div>
 </template>
 
 <script>
-import { useStore } from 'vuex'
 export default {
-  name: 'MyApp',
-  setup () {
-    const store = useStore()
-    const btnChangeName = () => {
-      store.commit('updateName')
-    }
-    return {
-      btnChangeName
-    }
-  }
+  name: 'MyApp'
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,14 @@
 import { createStore } from 'vuex'
 
+// 三个独立的模块
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
+
 export default createStore({
-  state: {
-    userName: 'zs'
-  },
-  getters: {
-    newName (state) {
-      return state.userName + '!!!'
-    }
-  },
-  mutations: {
-    updateName (state) {
-      state.userName = 'ls'
-    }
-  },
-  actions: {
-  },
   modules: {
+    cart,
+    user,
+    category
   }
 })
