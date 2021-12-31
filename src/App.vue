8d960cf0652d970240ba460eb1daf50dc8502a11
xyxx22 <2651288299@qq.com>
2021-12-31 13:06:07 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <div>app根组件</div>
+    <p>{{ $store.state.userName }}</p>
+    <p>{{ $store.getters.newName }}</p>
+    <button @click="btnChangeName">改变值</button>
   </div>
-  <router-view/>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'MyApp',
+  setup () {
+    const store = useStore()
+    const btnChangeName = () => {
+      store.commit('updateName')
+    }
+    return {
+      btnChangeName
     }
   }
 }
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    userName: 'zs'
+  },
+  getters: {
+    newName (state) {
+      return state.userName + '!!!'
+    }
   },
   mutations: {
+    updateName (state) {
+      state.userName = 'ls'
+    }
   },
   actions: {
   },
