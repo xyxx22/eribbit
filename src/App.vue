fae0a61597e7ff3367a35ff23d9c5add93fbb70c
xyxx22 <2651288299@qq.com>
2022-01-06 09:11:00 +0800

    加入路由规则

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-    APP根组件
-    <hr>
-    <button @click="fn">测试request工具函数</button>
-  </div>
+<RouterView></RouterView>
 </template>
-
-<script>
-import request from '@/utils/request.js'
-
-export default {
-  name: 'MyApp',
-  setup () {
-    const fn = () => {
-      request(' /member/profile', 'get', { a: 10 })
-    }
-
-    return { fn }
-  }
-}
-</script>
---
+++ src/views/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- 顶部通栏 -->
+  <!-- 头部组件 -->
+
+  <!-- 内容容器 -->
+  <div>容器</div>
+  <RouterView></RouterView>
+  <!-- 底部组件 -->
+
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style scoped lang='less'>
+
+</style>
